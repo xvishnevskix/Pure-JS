3aa28df0e0454a8d33388c6c5f032f436ab71a80
xvishnevskix <45593711+xvishnevskix@users.noreply.github.com>
2021-06-09 15:03:34 +0300

    Framework start (#2)
    * adding listeners
    
    * listeners

---
+++ src/components/table/Table.js
@@ -0,0 +1,107 @@
+import {ExcelComponent} from "../../core/ExcelComponent";
+
+export class Table extends ExcelComponent {
+    static className = 'excel__table'
+
+    toHTML() {
+        return `<div class="row">
+
+                <div class="row-info"></div>
+
+                <div class="row-data">
+                    <div class="column">
+                        A
+                    </div>
+                    <div class="column">
+                        B
+                    </div>
+                    <div class="column">
+                        C
+                    </div>
+                    <div class="column">
+                        A
+                    </div>
+                    <div class="column">
+                        B
+                    </div>
+                    <div class="column">
+                        C
+                    </div>
+                    <div class="column">
+                        A
+                    </div>
+                    <div class="column">
+                        B
+                    </div>
+                    <div class="column">
+                        C
+                    </div>
+                    <div class="column">
+                        A
+                    </div>
+                    <div class="column">
+                        B
+                    </div>
+                    <div class="column">
+                        C
+                    </div>
+                    <div class="column">
+                        A
+                    </div>
+                    <div class="column">
+                        B
+                    </div>
+                    <div class="column">
+                        C
+                    </div>
+                    <div class="column">
+                        A
+                    </div>
+                    <div class="column">
+                        B
+                    </div>
+                    <div class="column">
+                        C
+                    </div>
+                    <div class="column">
+                        A
+                    </div>
+                    <div class="column">
+                        B
+                    </div>
+                    <div class="column">
+                        C
+                    </div>
+                    <div class="column">
+                        A
+                    </div>
+                    <div class="column">
+                        B
+                    </div>
+                    <div class="column">
+                        C
+                    </div>
+                    <div class="column">
+                        A
+                    </div>
+                    <div class="column">
+                        B
+                    </div>
+                    <div class="column">
+                        C
+                    </div>
+                    <div class="column">
+                        A
+                    </div>
+                    <div class="column">
+                        B
+                    </div>
+                    <div class="column">
+                        C
+                    </div>
+
+                </div>
+
+            </div>`
+    }
+}
