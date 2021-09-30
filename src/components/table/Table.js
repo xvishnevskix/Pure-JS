7ae0cb5c08b0119ab562b4ce6692fee50001dd6a
xvishnevskix <45593711+xvishnevskix@users.noreply.github.com>
2021-09-30 14:09:11 +0300

    Creating new tables (#7)

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -20,7 +20,7 @@ export class Table extends ExcelComponent {
   }
 
   toHTML() {
-    return createTable(40, this.store.getState())
+    return createTable(20, this.store.getState())
   }
 
   prepare() {
